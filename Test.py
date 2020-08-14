d786a54af06618d55c914a617d74b2cf3b09e77d
yt0505 <daniel.kaiser@kit.edu>
2020-08-14 18:13:49 +0200

    Erste Versuche mit Flask

---
+++ Test.py
@@ -0,0 +1,3 @@
+from videocapture import *
+
+testt()
--- videocapture.py
+++ videocapture.py
@@ -3,18 +3,26 @@ import sys
 from main import *
 
 
-faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-video_capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
+def getBookData():
+	
+	faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+	video_capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
-Scan = True
-while Scan:
-    
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
+	Scan = True
+	while Scan:
+		
+		# Capture frame-by-frame
+		ret, frame = video_capture.read()
 
-    decodedObjects, Scan = decode(frame)
-    display(frame, decodedObjects)
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+		decodedObjects, Scan = decode(frame)
+		display(frame, decodedObjects)
+		
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+			
+def testt():
+	
+	while True:
+		print('soso')
+
